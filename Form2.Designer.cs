36ad0f06abaeaa5f4f6f80d7429047cd2262647b
kaanyvz <kaan_yavuz12@hotmail.com>
2024-05-19 00:31:00 +0300

    Initial commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace transaction
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components!= null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartSimulation = new System.Windows.Forms.Button();
+            this.textBoxTypeAUsers = new System.Windows.Forms.TextBox();
+            this.textBoxTypeBUsers = new System.Windows.Forms.TextBox();
+            this.comboBoxIsolationLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonStartSimulation
+            // 
+            this.buttonStartSimulation.Location = new System.Drawing.Point(12, 104);
+            this.buttonStartSimulation.Name = "buttonStartSimulation";
+            this.buttonStartSimulation.Size = new System.Drawing.Size(120, 23);
+            this.buttonStartSimulation.TabIndex = 0;
+            this.buttonStartSimulation.Text = "Start Simulation";
+            this.buttonStartSimulation.UseVisualStyleBackColor = true;
+            this.buttonStartSimulation.Click += new System.EventHandler(this.buttonStartSimulation_Click);
+            // 
+            // textBoxTypeAUsers
+            // 
+            this.textBoxTypeAUsers.Location = new System.Drawing.Point(12, 29);
+            this.textBoxTypeAUsers.Name = "textBoxTypeAUsers";
+            this.textBoxTypeAUsers.Size = new System.Drawing.Size(120, 20);
+            this.textBoxTypeAUsers.TabIndex = 1;
+            // 
+            // textBoxTypeBUsers
+            // 
+            this.textBoxTypeBUsers.Location = new System.Drawing.Point(12, 55);
+            this.textBoxTypeBUsers.Name = "textBoxTypeBUsers";
+            this.textBoxTypeBUsers.Size = new System.Drawing.Size(120, 20);
+            this.textBoxTypeBUsers.TabIndex = 2;
+            // 
+            // comboBoxIsolationLevel
+            // 
+            this.comboBoxIsolationLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxIsolationLevel.FormattingEnabled = true;
+            this.comboBoxIsolationLevel.Items.AddRange(new object[] {
+            "ReadUncommitted",
+            "ReadCommitted",
+            "RepeatableRead",
+            "Serializable",
+            "Snapshot"});
+            this.comboBoxIsolationLevel.Location = new System.Drawing.Point(12, 77);
+            this.comboBoxIsolationLevel.Name = "comboBoxIsolationLevel";
+            this.comboBoxIsolationLevel.Size = new System.Drawing.Size(120, 21);
+            this.comboBoxIsolationLevel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type A Users:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Type B Users:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Isolation Level:";
+            // 
+            // richTextBoxOutput
+            // 
+            this.richTextBoxOutput.Location = new System.Drawing.Point(12, 133);
+            this.richTextBoxOutput.Name = "richTextBoxOutput";
+            this.richTextBoxOutput.ReadOnly = true;
+            this.richTextBoxOutput.Size = new System.Drawing.Size(360, 240);
+            this.richTextBoxOutput.TabIndex = 7;
+            this.richTextBoxOutput.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 385);
+            this.Controls.Add(this.richTextBoxOutput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxIsolationLevel);
+            this.Controls.Add(this.textBoxTypeBUsers);
+            this.Controls.Add(this.textBoxTypeAUsers);
+            this.Controls.Add(this.buttonStartSimulation);
+            this.Name = "Form2";
+            this.Text = "Transaction Simulation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartSimulation;
+        private System.Windows.Forms.TextBox textBoxTypeAUsers;
+        private System.Windows.Forms.TextBox textBoxTypeBUsers;
+        private System.Windows.Forms.ComboBox comboBoxIsolationLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxOutput;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace transaction
+{
+    public partial class Form2 : Form
+    {
+        private string _connectionString = "Server=kypc;Database=AdventureWorks2022;Trusted_Connection=True;TrustServerCertificate=true;";
+        private IsolationLevel _isolationLevel;
+        private int _deadlockCount = 0; // Global deadlock count
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartSimulation_Click(object sender, EventArgs e)
+        {
+            _isolationLevel = (IsolationLevel)Enum.Parse(typeof(IsolationLevel), comboBoxIsolationLevel.SelectedItem.ToString());
+
+            int typeAUsers = int.Parse(textBoxTypeAUsers.Text);
+            int typeBUsers = int.Parse(textBoxTypeBUsers.Text);
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < typeAUsers; i++)
+            {
+                Thread thread = new Thread(() => TypeAUserThread());
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            for (int i = 0; i < typeBUsers; i++)
+            {
+                Thread thread = new Thread(() => TypeBUserThread());
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private void TypeAUserThread()
+        {
+            DateTime startTime = DateTime.Now;
+
+            for (int i = 0; i < 100; i++)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    SqlTransaction transaction = connection.BeginTransaction(_isolationLevel);
+
+                    try
+                    {
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteUpdateQuery(connection, transaction, "20110101", "20111231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteUpdateQuery(connection, transaction, "20120101", "20121231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteUpdateQuery(connection, transaction, "20130101", "20131231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteUpdateQuery(connection, transaction, "20140101", "20141231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteUpdateQuery(connection, transaction, "20150101", "20151231");
+                        }
+
+                        transaction.Commit();
+                        connection.Close();
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref _deadlockCount); // Increment global deadlock count
+                        // Handle error
+                    }
+                }
+            }
+
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("Type A User Thread finished. Deadlocks: {0}, Elapsed Time: {1}", _deadlockCount, elapsedTime);
+        }
+
+        private void TypeBUserThread()
+        {
+            DateTime startTime = DateTime.Now;
+
+            for (int i = 0; i < 100; i++)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    SqlTransaction transaction = connection.BeginTransaction(_isolationLevel);
+
+                    try
+                    {
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteSelectQuery(connection, transaction, "20110101", "20111231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteSelectQuery(connection, transaction, "20120101", "20121231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteSelectQuery(connection, transaction, "20130101", "20131231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteSelectQuery(connection, transaction, "20140101", "20141231");
+                        }
+                        if (new Random().NextDouble() < 0.5)
+                        {
+                            ExecuteSelectQuery(connection, transaction, "20150101", "20151231");
+                        }
+
+                        transaction.Commit();
+                        connection.Close();
+
+                        
+                    }
+                    catch (SqlException ex)
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref _deadlockCount); // Increment global deadlock count
+                        // Handle error
+                    }
+                }
+            }
+
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            Console.WriteLine("Type B User Thread finished. Deadlocks: {0}, Elapsed Time: {1}", _deadlockCount, elapsedTime);
+        }
+
+        private void ExecuteUpdateQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            using (SqlCommand command = new SqlCommand("UPDATE Sales.SalesOrderDetail SET UnitPrice = UnitPrice * 10.0 / 10.0 WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)", connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 1205) // Deadlock
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref _deadlockCount); // Increment global deadlock count
+                        ExecuteUpdateQuery(connection, transaction, beginDate, endDate);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        private void ExecuteSelectQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            using (SqlCommand command = new SqlCommand("SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)", connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+
+                try
+                {
+                    command.ExecuteScalar();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 1205) // Deadlock
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref _deadlockCount); // Increment global deadlock count
+                        ExecuteSelectQuery(connection, transaction, beginDate, endDate);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
